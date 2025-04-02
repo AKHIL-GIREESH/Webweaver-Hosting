3fc39e19c0650743e27cdd019316cc58fbf73cb3
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-04-02 16:04:18 +0530

    added sh

---
+++ initializer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+exec > >(tee -i /var/log/user-data.log)
+exec 2>&1
+sudo apt update -y
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+sudo apt install git -y 
+
+ansible-playbook -i localhost ansible.yml
