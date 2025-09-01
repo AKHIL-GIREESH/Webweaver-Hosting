570f55a9b0d0d2c5abb2b3bd22eacace06765aa2
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-09-01 16:00:02 +0530

    deployment get

--- controllers/deployments.js
+++ controllers/deployments.js
@@ -3,7 +3,7 @@ const Hosting = require("../models/hostingSchema");
 const getDeploymentsByAuthor = async (req, res) => {
   try {
     const { author } = req.params;
-    const deployments = await Hosting.find({ author });
+    const deployments = await Hosting.find({ author: author });
     res.status(200).json(deployments);
   } catch (err) {
     console.error("Error fetching deployments by author:", err);
